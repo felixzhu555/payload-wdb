f712e09652a59539c14f5407194c5fddb721fd70
Nicholas Shin <nshin02@berkeley.edu>
2023-11-07 17:34:51 -0800

    Set up Data Export

---
+++ packages/payload/src/dataExports/dataExportsCollection.ts
@@ -0,0 +1,62 @@
+//user permissions and access for data export???
+import type { CollectionConfig } from '../collections/config/types'
+import type { Access, Config } from '../config/types'
+
+import exportHandler from './requestHandlers/export'
+
+const dataExportAcess: Access = ({ req }) => ({
+  'user.value': {
+    equals: req?.user?.id,
+  },
+})
+
+const getDataExportsCollection = (config: Config): CollectionConfig => ({
+  access: {
+    delete: dataExportAcess,
+    read: dataExportAcess,
+  },
+  admin: {
+    hidden: true,
+  },
+  endpoints: [
+    {
+      handler: exportHandler,
+      method: 'post',
+      path: '/export',
+    },
+  ],
+  fields: [
+    {
+      name: 'user',
+      hooks: {
+        beforeValidate: [
+          ({ req }) => {
+            if (!req?.user) {
+              return null
+            }
+            return {
+              relationTo: req?.user.collection,
+              value: req?.user.id,
+            }
+          },
+        ],
+      },
+      relationTo: config.collections
+        .filter((collectionConfig) => collectionConfig.auth)
+        .map((collectionConfig) => collectionConfig.slug),
+      required: true,
+      type: 'relationship',
+    },
+    {
+      name: 'key',
+      type: 'text',
+    },
+    {
+      name: 'value',
+      type: 'json',
+    },
+  ],
+  slug: 'payload-preferences',
+})
+
+export default getDataExportsCollection
