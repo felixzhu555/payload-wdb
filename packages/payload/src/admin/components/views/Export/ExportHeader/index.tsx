471d74095a4ae30997eab704654020708ac95260
Nicholas Shin <nshin02@berkeley.edu>
2023-11-16 16:43:21 -0800

    merge frontend backend working, button downloads hardcoded collections

---
+++ packages/payload/src/admin/components/views/Export/ExportHeader/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+import './index.scss'
+
+const ExportHeader = () => {
+  return (
+    <React.Fragment>
+      <div className="header">
+        <div className="title">Export</div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default ExportHeader
--- packages/payload/src/admin/components/views/Export/index.tsx
+++ packages/payload/src/admin/components/views/Export/index.tsx
@@ -4,15 +4,14 @@ import { useTranslation } from 'react-i18next'
 import type { Collection } from '../../../../exports/types'
 
 import { SanitizedCollectionConfig } from '../../../../exports/types'
-import { useAuth } from '../../utilities/Auth'
 import { useConfig } from '../../utilities/Config'
+import ExportList from './ExportList/index'
 
 type ExportType = {
   collections: Array<Collection>
 }
 
 const Export = () => {
-  const { permissions, user } = useAuth()
   const {
     admin: {
       components: { afterNavLinks, beforeNavLinks },
@@ -23,87 +22,92 @@ const Export = () => {
     serverURL,
   } = useConfig()
   const { i18n } = useTranslation('general')
-  const [data, setData] = useState<ExportType>()
+  const [collectionsDict, setCollectionsDict] = useState({})
+  const [dataFromChild, setDataFromChild] = useState(null)
 
-  const exportData = () => {
-    // TODO: not finished
-    collections.forEach((collection) => {
-      console.log(collection.slug)
-      ;(async function () {
-        const data = await fetch(`${serverURL}${api}/${collection.slug}`, {
+  const handleDataFromChild = (data) => {
+    // Process the data received from the child component
+    setDataFromChild(data)
+  }
+
+  const generateRandomVersions = (count) => {
+    const randomVersions = []
+    const generateUniqueVersion = () => {
+      const version = `${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}`
+      return randomVersions.includes(version) ? generateUniqueVersion() : version
+    }
+    for (let i = 1; i <= count; i++) {
+      const uniqueVersion = generateUniqueVersion()
+      randomVersions.push(uniqueVersion)
+    }
+    return randomVersions.sort((a, b) => parseFloat(a) - parseFloat(b))
+  }
+
+  function getRandomCountWithDistribution() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1
+    if (randomNumber <= 10) {
+      return 1 // 10%
+    } else if (randomNumber <= 35) {
+      return 2 // 25%
+    } else if (randomNumber <= 65) {
+      return 3 // 30%
+    } else if (randomNumber <= 85) {
+      return 4 // 20%
+    } else {
+      return 5 // 15%
+    }
+  }
+
+  const exportData = async () => {
+    const newData = {}
+    for (const collection of collections) {
+      try {
+        const response = await fetch(`${serverURL}${api}/${collection.slug}`, {
           credentials: 'include',
           headers: {
             'Accept-Language': i18n.language,
           },
         })
-
-        return await data.json()
-      })()
-        .then((res) => {
-          console.log(collection.slug, res)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    })
+        const data = await response.json()
+        newData[collection.slug] = data
+      } catch (error) {
+        console.error(`Error fetching data for ${collection.slug}:`, error)
+      }
+    }
   }
 
   useEffect(() => {
-    // exportData()
-    ;(async function () {
-      const data = await fetch(
-        `${serverURL}/api/data-exports?collections={"users": ["1.1", "1.2"], "posts": ["1.1"]}`,
-        {
-          credentials: 'include',
-          headers: {
-            'Accept-Language': i18n.language,
-          },
-          method: 'POST',
-        },
-      )
-      async function downloadFromStream(stream) {
-        // Convert the ReadableStream into a Blob
-        const blob = await new Response(stream).blob()
-
-        // Create a URL for the Blob
-        const url = URL.createObjectURL(blob)
-
-        // Create a temporary anchor element
-        const a = document.createElement('a')
-        a.href = url
-
-        // Set a filename for the downloaded file
-        const currentDateTime = new Date()
-        const dateTimeString = currentDateTime.toString()
-        a.download = dateTimeString + '.json'
-
-        // Append the anchor to the document
-        document.body.appendChild(a)
-
-        // Trigger the download
-        a.click()
-
-        // Clean up by revoking the URL and removing the anchor element
-        URL.revokeObjectURL(url)
-        document.body.removeChild(a)
-      }
-      downloadFromStream(data.body)
-      const newData = await data.body
-      console.log(data.body)
-    })()
+    exportData()
   }, [])
 
-  return (
-    <React.Fragment>
-      LIST:
-      {collections.map((collection) => (
-        <React.Fragment>
-          <br />
-          Collection name: {collection.slug}
-        </React.Fragment>
-      ))}
-    </React.Fragment>
-  )
+  useEffect(() => {
+    const updatedCollectionsDict = {}
+    for (const collection of collections) {
+      if (collection.labels && collection.labels.plural) {
+        const tempKey = collection.labels.plural
+        const tempBucket = {}
+        tempBucket['slug'] = collection.slug
+        const randomCount = getRandomCountWithDistribution()
+        tempBucket['versions'] = generateRandomVersions(randomCount) // Change the argument to generate more or fewer versions
+        updatedCollectionsDict[tempKey] = tempBucket
+      }
+    }
+    setCollectionsDict(updatedCollectionsDict)
+  }, [collections])
+
+  if (collectionsDict && Object.keys(collectionsDict).length > 0) {
+    return (
+      <React.Fragment>
+        <div>
+          <div>
+            <ExportList collectionsDict={collectionsDict} onDataChange={handleDataFromChild} />
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  } else {
+    return <p>Fetching Data...</p>
+  }
 }
 
 export default Export
